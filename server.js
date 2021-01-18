033f02f2d4c97d68a36251a5b00478c62f045e73
Michail-Shin <shin.michail@gmail.com>
2021-01-18 16:33:24 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'michailshin',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> { res.send(database.users) });
+
+app.post('/signin', signin.handleSignIn(db, bcrypt));
+
+app.post('/register', register.handleRegister(db, bcrypt));
+
+app.get('/profile/:id', profile.handleProfileGet(db));
+
+app.put('/image', image.handleImage(db));
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+app.listen('3000', ()=> {
+	console.log('App is running on port 3000');
+});
