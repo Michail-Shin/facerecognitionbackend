39a57511b9044c588630ce6404f6de14a7f141ba
Michail-Shin <shin.michail@gmail.com>
2021-01-18 18:41:41 +0300

    updated database destination

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-aerodynamic-08805',
     user : 'michailshin',
     password : '',
     database : 'smart-brain'
