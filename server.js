212fd423cdb236950fdb4418aa1ad928bbc74e8a
Michail-Shin <shin.michail@gmail.com>
2021-01-18 18:46:02 +0300

    fixed database settings

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-aerodynamic-08805',
-    user : 'michailshin',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
